68139b075082cb70f1cf77e561e531a82b8a2c5d
SDubrovskis <78731640+MikaHvostenberg@users.noreply.github.com>
2024-05-04 15:37:27 +0200

    measuring steady flow as a function of prob. distribution

--- simple_randomwalk.py
+++ simple_randomwalk.py
@@ -41,10 +41,11 @@ def statistics(N:int,particles:int,steps:int):
 #statistics(domains,particles,750)
 #here is the function for the stationary flow of the particles
 
-def stationary_flow(N:int,particles:int,steps:int):
+def stationary_flow(N:int,particles:int,steps:int,weights:list,graph=True):
+    '''''This function simulates the stationary flow through the probabilistic setup and calculates
+    the average heat flow (over the last 100 seconds)'''''
      #here the key parameters are defined
     walks=[-1,0,1]
-    weights=[0.25,0.5,0.25]
     flow=np.zeros(steps)
     #here we define the initial destribution
     current=np.zeros(N)
@@ -65,9 +66,28 @@ def stationary_flow(N:int,particles:int,steps:int):
         current=tmp
     avg_flow=np.average(flow[-100:])
     print(avg_flow)
-    plt.plot(flow)
-    plt.grid()
-    plt.show()
-    return current
+    #printing block(makes a graph of a single measurement)
+    if graph==True:
+        plt.plot(flow,color="black",label="momentary particle flux")
+        plt.grid()
+        plt.axhline(y=avg_flow,color='red',linestyle='dashed',label=f"running average: {avg_flow} ")
+        plt.legend()
+        plt.show()
+    return avg_flow
+#draws a single thing
+#stationary_flow(10,10**5,200,[0.25,0.5,0.25])
 
-stationary_flow(10,10**5,300)
+#this is another experimental function
+def measure_flow(N=10):
+    """"Fucntion that measures the stable heat flow as a function of the probabilities of 
+    particles going left or right"""
+    #target array
+    steady_flow=[]
+    probabilities=np.linspace(0,0.5,N)
+    for prob in probabilities:
+        steady_flow.append(stationary_flow(10,10**5,200,[prob,1-2*prob,prob],graph=False))
+    plt.plot(probabilities,steady_flow)
+    plt.show()
+    return
+#lets make a graph of the flow as a function of probabilities
+measure_flow()
