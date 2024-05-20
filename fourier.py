2fd0f863027c3803adcd07a76668abafcccffe2f
dngp <markuss.kenins@gmail.com>
2024-05-20 19:36:43 +0200

    Amended for a constant initial condition

--- fourier.py
+++ fourier.py
@@ -12,16 +12,27 @@ if __name__ == "__main__":
         # coeffs.append(2/(n*math.pi)*(5-6*math.sin(n*math.pi/6)+math.cos(n*math.pi)))
         # coeffs.append(2/(n*math.pi)*(6-6*math.sin(n*math.pi/6)))
         
-        # initial condition LAMBDA(x) = -6*12^2 (x-1/12)^2 + 6 
-        I_lam = -24/(n*math.pi)**3*(-12+12*math.cos(n*math.pi/6)+n*math.pi*math.sin(n*math.pi/6))
+        # initial condition LAMBDA(x) = 11/12
+        I_lam = 11/(12*n*math.pi)*(1-math.cos(n*math.pi/6))
+        
+        # initial condition LAMBDA(x) = -(11/8)*12^2 (x-1/12)^2 + 11/8 
+        # I_lam = -24/(n*math.pi)**3*(-12+12*math.cos(n*math.pi/6)+n*math.pi*math.sin(n*math.pi/6))
+        
         I_s = 1/(n*math.pi)
         coeffs.append(2*(I_lam - I_s))
     
-    timerangeone = np.linspace(0,2,40) #small list! not the axis
-    timerangetwo = np.linspace(2,5,40)
-    timerangethree = np.linspace(5,20,80)
-    timerangefour = np.linspace(20,100,40)
-    timetotal = np.concatenate((timerangeone,timerangetwo,timerangethree,timerangefour),axis=None)
+    #small list! not the axis
+    timetotal = np.concatenate(
+        (
+            np.linspace(0,1,40),
+            np.linspace(1,5,80),
+            np.linspace(5,10,40),
+            np.linspace(10,20,40),
+            np.linspace(20,50,80),
+            np.linspace(50,100,40),
+        ),
+        axis=None
+    )
 
     xrange = np.linspace(0,1,1000)
     param_D = 0.005
@@ -32,7 +43,7 @@ if __name__ == "__main__":
     # ax.set_ylim(bottom=0,top=6)
     # ax.set_xlim(left=0,right=1)
     ax.set_xticks([0,1/6,2/6,3/6,4/6,5/6,1],labels=["0","1/6","2/6","3/6","4/6","5/6","1"])
-    ax.set_title("$T(x,t)$ for $0<t<100$, $D=$" + f"{param_D}, expanded to {numterms} terms")    
+    ax.set_title(f"$T(x,t)$ for $0<t<100$, $D= {param_D}$, expanded to {numterms} terms\nwith $\Lambda(x)=11/12$ over $[0,1/6]$")    
     ax.set_xlabel("$x$")
     ax.set_ylabel("$T(x,t)$")
 
@@ -44,5 +55,5 @@ if __name__ == "__main__":
 
         ax.plot(xrange,solvals,c=color[j])
     
-    fig.savefig("Tplot-parabolic.pdf")
+    fig.savefig("initialplot-parabolic.pdf")
     plt.show()
